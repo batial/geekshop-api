d7d8d2ceabd28bb96c5ce59f98ed98ce24a9ecc1
Sebastian <sebastian.debesa@hotmail.com>
2026-03-23 10:30:27 -0300

    initial project setup

---
+++ src/main/java/batial/geekshop/GeekShopApplication.java
@@ -0,0 +1,13 @@
+package batial.geekshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/batial/geekshop/GeekShopApplicationTests.java
@@ -0,0 +1,13 @@
+package batial.geekshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
